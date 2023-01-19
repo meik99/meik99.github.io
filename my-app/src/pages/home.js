658b842fc37858af984732b145e30b7e55e3a392
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2023-01-19 10:32:26 +0100

    website redesign

--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -1,11 +1,15 @@
 import "./home.scss";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import settings from "../assets/img/coffeebank/settings_thumbnail.png";
+import { useCallback } from "react";
 
 function Home() {
+    const navigate = useNavigate();
+    const coffeeLink = useCallback(() => navigate("/coffeebank"), [navigate]);
+
     return (
         <div className="container">
-            <div className="card mt-5 mb-5">
+            <div className="card mt-4" onClick={coffeeLink}>
                 <div className="row g-0">
                     <div className="col-md-2">
                         <img src={settings} className="img-fluid rounded-start" alt="..." />
@@ -21,13 +25,20 @@ function Home() {
                     </div>
                 </div>
             </div>
-            <div className="row mb-5">
-                <div className="col-12">
-                    <h3>CoffeeBank</h3>
-                    <p>
-                        An Android app to manage customers, products and transactions.
-                    </p>
-                    <NavLink className="btn">Details</NavLink>
+            <div className="card mt-4">
+                <div className="row g-0">
+                    <div className="col-md-2">
+                        <img src={settings} className="img-fluid rounded-start" alt="..." />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">CoffeeBank</h5>
+                            <p className="card-text">
+                                An Android app to manage customers, products and transactions.
+                            </p>
+                            <NavLink className="btn btn-outline-dark" to="/coffeebank">Details</NavLink>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
